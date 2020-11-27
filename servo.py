7f0fce90e02e23268658ef4e4e2ff8edc267466f
diulamax <linweizhuo1996@gmail.com>
2020-11-27 15:09:46 -0800

    update function

--- servo.py
+++ servo.py
@@ -3,6 +3,12 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
+DELAY = 0.01
+PIN_COIL_A1 = 13  # IN_1
+PIN_COIL_A2 = 15  # IN_2
+PIN_COIL_B1 = 16  # IN_3
+PIN_COIL_B2 = 18  # IN_4
+
 class Servo(object):
     def __init__(self,PIN_COIL_A1, PIN_COIL_A2, PIN_COIL_B1, PIN_COIL_B2, DELAY=0.3):
         self.PIN_COIL_A1 = PIN_COIL_A1
@@ -110,8 +116,19 @@ class Servo(object):
                 else:
                     j = i
                 seq = self.switcher.get(self.half_step_list_ccw[j])
-                print(seq)
                 seq()
+    
+    def trace(self, pixels, center=225, kp=0.1, threshold=10):
+        error = pixels - center
+        if abs(error) < threshold:
+            print("Reached")
+        else:
+            print("Moving to traget" + "\n")
+            print("Error: " + str(error))
+            # Multiple by -1 if the direction is reversed
+            step = int(kp * error) * -1
+            print("Step: " + str(step))
+            self.move(step)
     
     def stop(self):
         GPIO.cleanup()
@@ -119,13 +136,8 @@ class Servo(object):
         
         
 if __name__ == "__main__":
-    DELAY = 0.3
-    PIN_COIL_A1 = 13  # IN_1
-    PIN_COIL_A2 = 15  # IN_2
-    PIN_COIL_B1 = 18  # IN_3
-    PIN_COIL_B2 = 22  # IN_4
     servo = Servo(PIN_COIL_A1, PIN_COIL_A2, PIN_COIL_B1, PIN_COIL_B2, DELAY)
-    servo.move(-10)
-    time.sleep(0.1)
+    servo.move(2000)
+    time.sleep(0.5)
     servo.stop()
     
